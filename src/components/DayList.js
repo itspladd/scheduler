aa2f3ee7f4182b768b1480c44b10842b32e6ee18
Paul Ladd <vlendle@gmail.com>
2021-04-12 21:11:07 -0500

    Add state to app, finish dayList, add stories for dayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,21 @@
 import React from "react";
+import DayListItem from "./DayListItem"
 
 export default function DayList(props) {
-  const { days, day, setDay } = props;
 
-  
+  const dayListItems = props.days.map(day => {
+    return <DayListItem
+      key = {day.id}
+      name = {day.name} 
+      spots = {day.spots}
+      selected = {day.name === props.day}
+      setDay = {props.setDay} 
+    />
+  });
+
   return(
-    <li>
-      {props.spots} spots remaining
-    </li>
+    <ul>
+      {dayListItems}
+    </ul>
   ) 
 }
