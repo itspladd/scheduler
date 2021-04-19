0fb0d8b3b89c48d9a453e308d49102dcf1b05cda
Paul Ladd <vlendle@gmail.com>
2021-04-19 17:04:23 -0500

    Finish testing for Form component

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -4,31 +4,41 @@ import Button from "../Button";
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
+  const [error, setError] = useState("");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   // Resets the information fields
   const reset = () => {
     setInterviewer(null);
+    setError("")
     setName("");
   }
 
   // Cancel handler - call reset and onCancel from props
   const cancel = () => {
+
+    console.log("NAME:", name)
     reset();
     props.onCancel();
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const {student, interviewer} = event;
-    props.onSave(student, interviewer);
+  const handleSubmit = () => {
+    if(!name) {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if(!interviewer) {
+      setError("You must select an interviewer!")
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
   };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off"
-          onSubmit={handleSubmit}>
+        <form autoComplete="off">
           <input
             data-testid = "student-name-input"
             className="appointment__create-input text--semi-bold"
@@ -39,12 +49,13 @@ export default function Form(props) {
             onChange={event => setName(event.target.value)}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={handleSubmit}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -28,8 +28,19 @@ describe("Form", () => {
     );
     expect(getByTestId("student-name-input")).toHaveValue("Perkins the Ferret");
   });
-  
+
   it("validates that the student name is not blank", () => {
+
+    const onSave = jest.fn();
+
+    const {getByText} = render(
+      <Form
+        onSave={onSave}
+        interviewers={interviewers}
+      />
+    );
+    
+    fireEvent.click(getByText("Save"));
     /* 1. validation is shown */
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
   
@@ -37,14 +48,52 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  it("calls onSave function when the name is defined", () => {
-    /* 3. validation is not shown */
+  it("can successfully save after trying to submit an empty student name", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} interviewer={1} onSave={onSave} />
+    );
+  
+    fireEvent.click(getByText("Save"));
+  
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Save"));
+  
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
-    /* 4. onSave is called once*/
     expect(onSave).toHaveBeenCalledTimes(1);
-  
-    /* 5. onSave is called with the correct arguments */
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
+
+  it("calls onCancel and resets the input field", () => {
+    const onCancel = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        onSave={jest.fn()}
+        onCancel={onCancel}
+      />
+    );
+  
+    fireEvent.click(getByText("Save"));
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Cancel"));
+  
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+  
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
+
 })
