14482179b54a7aa0b7bdf78526f91dd9bbdfeb03
Paul Ladd <vlendle@gmail.com>
2021-04-21 18:20:27 -0500

    Add the final cypress test for deleting appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -28,16 +28,44 @@ describe("Appointments", () => {
   });
   it("should edit an interview", () => {
     // 1. Click on the "Edit" button in an existing appt
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .get("[alt=Edit]")
+      .click({force:true})
+
     // 2. Change the name 
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type('Smillerton Frunglebuns')
+
     // 3. Change the interviewer
+    cy.get("[alt='Tori Malcolm']")
+      .click()
+
     // 4. Click the save button
-    // 5. See a booked appt
+    cy.contains("Save")
+      .click()
+
+    // 5. See the updated appt
+    cy.contains(".appointment__card--show", "Smillerton Frunglebuns")
+      .contains(".appointment__card--show", "Tori Malcolm")
 
   });
   it("should delete an interview", () => {
     // 1. Click on the "Delete" button in an existing appt
-    // 2. Click the Confirm button
-    // 3. See the slot is empty
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .get("[alt=Delete]")
+    .click({force:true})
 
+    // 2. Click the Confirm button
+    cy.contains("Confirm")
+    .click()
+
+    // 3. See the slot is empty
+    cy.contains("Deleting")
+
+    cy.contains("Deleting").should("not.exist")
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist")
   });
 });
