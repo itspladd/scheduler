63d1f74723e02677f6a42c39e450ba4e11d531d1
Paul Ladd <vlendle@gmail.com>
2021-04-21 14:13:57 -0500

    Add database reset to the beginning of each test. Add remainder of interview booking test.;

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,14 +1,32 @@
 describe("Appointments", () => {
   it("should book an interview", () => {
+    cy.request("GET", "/api/debug/reset")
     cy.visit("/")
       .contains("Monday")
+    
     // 1. Click on the "Add" button in the second appt
+    cy.get("[alt=Add]")
+      .first()
+      .click()
+
     // 2. Enter a name
+    cy.get('[data-testid=student-name-input]')
+      .type('Rosenford Gerald-Krantz III')
+
     // 3. Choose an interviewer
+    cy.get("[alt='Sylvia Palmer']")
+      .click()
+
     // 4. Click the save button
+    cy.contains("Save")
+      .click()
+
     // 5. See a booked appt
+    cy.contains("Rosenford Gerald-Krantz III")
   });
   it("should edit an interview", () => {
+    cy.request("GET", "/api/debug/reset")
+
     cy.visit("/")
       .contains("Monday")
     // 1. Click on the "Edit" button in an existing appt
@@ -19,6 +37,8 @@ describe("Appointments", () => {
 
   });
   it("should delete an interview", () => {
+    cy.request("GET", "/api/debug/reset")
+
     cy.visit("/")
       .contains("Monday")
     // 1. Click on the "Delete" button in an existing appt
